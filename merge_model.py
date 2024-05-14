a5dbeb7a0b1ce6e37cd4e0e5156d7c99bba6dd89
BigsnarfDude <bigsnarfdude@users.noreply.github.com>
2024-05-14 11:06:52 -0600

    Create merge_model.py

---
+++ merge_model.py
@@ -0,0 +1,12 @@
+import torch
+from peft import AutoPeftModelForCausalLM
+
+# Load PEFT model on CPU
+model = AutoPeftModelForCausalLM.from_pretrained(
+    output_dir,
+    torch_dtype=torch.float16,
+    low_cpu_mem_usage=True,
+)
+
+merged_model = model.merge_and_unload()
+merged_model.save_pretrained("./merged_llama-3-70b-hf-no-robot",safe_serialization=True, max_shard_size="2GB")
